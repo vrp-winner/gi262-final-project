09aceb54502d52e212713ab000165d906c9dc5f2
Brocoliii <puree.suwa@bumail.net>
2025-11-20 21:51:47 +0700

    Update font asset and add new UI prefabs
    Modified the iannnnn-DOG-Bold SDF font asset to update glyph and character tables. Added new image and textbox prefabs with associated meta files. Updated MainMenu and TestBG_BossFightScene 1 Unity scenes, and made changes to the Link.cs UI script.

--- Assets/_Final Project/Scripts/UI/Link.cs
+++ Assets/_Final Project/Scripts/UI/Link.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Link : MonoBehaviour
 {
-   
+    public AudioSource myAudio;
+    public AudioClip meowSound;
+
     public void OpenLink(string urlToOpen)
     {
+
       
         if (!string.IsNullOrEmpty(urlToOpen))
         {
          
             Application.OpenURL(urlToOpen);
+            myAudio.PlayOneShot(meowSound);
         }
       
     }
